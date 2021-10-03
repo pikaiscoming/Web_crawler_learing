588d1a0cea670c9e6eb0fc0d65f2a5c8aef64621
pikaiscoming <48316662+pikaiscoming@users.noreply.github.com>
2021-10-03 16:44:41 +0800

    Add files via upload

---
+++ ch 11 ajax.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  3 12:24:13 2021
+
+@author: pikachu
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+driver = webdriver.PhantomJS()
+
+driver.get('http://pythonscraping.com/pages/javascript/ajaxDemo.html')
+try:
+    element = WebDriverWait(driver, 10).until(
+                       EC.presence_of_element_located((By.ID, 'loadedButton')))
+finally:
+    print(driver.find_element_by_id('content').text)
+    driver.close()
